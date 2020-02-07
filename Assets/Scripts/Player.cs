34b575fe5b42079f439329791ec8c39270eb5b43
jfcarocota <jfcarocota@gmail.com>
2020-02-07 14:31:19 -0700

    aniamtions controller 1

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,14 +7,32 @@ public class Player : MonoBehaviour
     [SerializeField]
     float moveSpeed = 3f;
 
+    SpriteRenderer spr;
+
+    Animator anim; 
+
+    void Awake()
+    {
+        spr = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+    }
+
     Vector2 Axis
     {
         get => new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+    }
+
+    bool FlipSprite
+    {
+        get => Axis.x > 0 ? false : Axis.x < 0 ? true : spr.flipX;
     }
 
     //Una vez por frame
     void Update()
     {
         transform.Translate(Axis * moveSpeed * Time.deltaTime);
+        spr.flipX = FlipSprite;
+
+        anim.SetFloat("axisX", Mathf.Abs(Axis.x));
     }
 }
