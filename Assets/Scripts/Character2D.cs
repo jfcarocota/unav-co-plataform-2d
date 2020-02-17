63e4279d3156a8060c10c2e9784c23f1039ad943
jfcarocota <jfcarocota@gmail.com>
2020-02-17 08:52:28 -0700

    base class character

---
+++ Assets/Scripts/Character2D.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character2D : MonoBehaviour
+{
+    [SerializeField, Range(0.1f, 7f)]
+    protected float moveSpeed = 3f;
+    [SerializeField, Range(0.1f, 10f)]
+    protected float jumpForce = 7f;
+
+    protected SpriteRenderer spr;
+
+    protected Animator anim; 
+
+    protected Rigidbody2D rb2d;
+
+    [SerializeField]
+    protected Color rayColor = Color.magenta;
+    [SerializeField, Range(0.1f, 5f)]
+    protected float rayDistance = 2f;
+    [SerializeField]
+    protected LayerMask groundLayer;
+
+    void Awake()
+    {
+        spr = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    protected Vector2 Axis
+    {
+        get => new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+    }
+
+    protected bool FlipSprite
+    {
+        get => Axis.x > 0 ? false : Axis.x < 0 ? true : spr.flipX;
+    }
+
+    protected bool JumpButton
+    {
+        get => Input.GetButtonDown("Jump");
+    }
+
+    protected bool Grounding
+    {
+        get => Physics2D.Raycast(transform.position, Vector2.down, rayDistance, groundLayer);
+    }
+
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,53 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player : Character2D
 {
-    [SerializeField, Range(0.1f, 7f)]
-    float moveSpeed = 3f;
-    [SerializeField, Range(0.1f, 10f)]
-    float jumpForce = 7f;
-
-    SpriteRenderer spr;
-
-    Animator anim; 
-
-    Rigidbody2D rb2d;
-
-    [SerializeField]
-    Color rayColor = Color.magenta;
-    [SerializeField, Range(0.1f, 5f)]
-    float rayDistance = 2f;
-    [SerializeField]
-    LayerMask groundLayer;
-
-    void Awake()
-    {
-        spr = GetComponent<SpriteRenderer>();
-        anim = GetComponent<Animator>();
-        rb2d = GetComponent<Rigidbody2D>();
-    }
-
-    Vector2 Axis
-    {
-        get => new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-    }
-
-    bool FlipSprite
-    {
-        get => Axis.x > 0 ? false : Axis.x < 0 ? true : spr.flipX;
-    }
-
-    bool JumpButton
-    {
-        get => Input.GetButtonDown("Jump");
-    }
-
-    bool Grounding
-    {
-        get => Physics2D.Raycast(transform.position, Vector2.down, rayDistance, groundLayer);
-    }
-
     //Una vez por frame
     void Update()
     {
